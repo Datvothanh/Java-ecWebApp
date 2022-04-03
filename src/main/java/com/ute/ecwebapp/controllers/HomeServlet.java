d9d437d3c25c68cbef32ead72904b53be831a680
Thành Đạt <93526680+Datvothanh@users.noreply.github.com>
2022-04-03 09:47:23 +0700

    Initial commit - Võ Thành Đạt

---
+++ src/main/java/com/ute/ecwebapp/controllers/HomeServlet.java
@@ -0,0 +1,19 @@
+package com.ute.ecwebapp.controllers;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "HomeServlet", value = "/Home/*")
+public class HomeServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
